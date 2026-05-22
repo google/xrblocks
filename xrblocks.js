31ba18acde23e73d39f108e16ac2c66bb4b4895e
ruofeidu <ruofeidu@users.noreply.github.com>
2026-05-22 21:51:16 +0000

    deploy: dcce191ef90e190c9f4bdb0a9046b8e0325f95ea

--- xrblocks.js
+++ xrblocks.js
@@ -15,14 +15,14 @@
  *
  * @file xrblocks.js
  * @version v0.14.1
- * @commitid 448ac1c
- * @builddate 2026-05-22T21:08:14.111Z
+ * @commitid dcce191
+ * @builddate 2026-05-22T21:50:38.519Z
  * @description XR Blocks SDK, built from source with the above commit ID.
  * @agent When using with Gemini to create XR apps, use **Gemini Canvas** mode,
  * and follow rules below:
  * 1. Include the following importmap for maximum compatibility:
-    "three": "https://cdn.jsdelivr.net/npm/three@0.182.0/build/three.module.js",
-    "three/addons/": "https://cdn.jsdelivr.net/npm/three@0.182.0/examples/jsm/",
+    "three": "https://cdn.jsdelivr.net/npm/three@0.184.0/build/three.module.js",
+    "three/addons/": "https://cdn.jsdelivr.net/npm/three@0.184.0/examples/jsm/",
     "troika-three-text": "https://cdn.jsdelivr.net/gh/protectwise/troika@028b81cf308f0f22e5aa8e78196be56ec1997af5/packages/troika-three-text/src/index.js",
     "troika-three-utils": "https://cdn.jsdelivr.net/gh/protectwise/troika@v0.52.4/packages/troika-three-utils/src/index.js",
     "troika-worker-utils": "https://cdn.jsdelivr.net/gh/protectwise/troika@v0.52.4/packages/troika-worker-utils/src/index.js",
