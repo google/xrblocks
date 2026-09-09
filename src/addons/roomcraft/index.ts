73c57987b3bfabd107bc1c48792f448c24f5e5fc
Salman Muin Kayser Chishti <13schishti@gmail.com>
2026-09-09 20:22:33 +0100

    Introduce scene authoring: public exports

---
+++ src/addons/roomcraft/index.ts
@@ -0,0 +1,25 @@
+export {Roomcraft} from './Roomcraft';
+export {createDefaultCatalog, createModelAsset} from './Catalog';
+export type {ModelAssetOptions} from './Catalog';
+export {SCENE_PLAN_SCHEMA, buildScenePrompt} from './ScenePlan';
+export {
+  MAX_SCENE_DISTANCE,
+  MAX_SCENE_OBJECTS,
+  MAX_SCENE_SCALE,
+  MIN_SCENE_SCALE,
+} from './SceneTypes';
+export type {
+  RoomcraftEventMap,
+  RoomcraftOptions,
+  RoomcraftStatus,
+  SceneAsset,
+  SceneAssetDescription,
+  SceneEdit,
+  SceneLayout,
+  SceneObject,
+  SceneObjectChanges,
+  ScenePlan,
+  ScenePlanner,
+  SceneRequest,
+  SceneVector3,
+} from './SceneTypes';
